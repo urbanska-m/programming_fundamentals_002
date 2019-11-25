ab6b0aec5533b77a7bc1ee4c0bbf3b9f690db208
Monika Urbanska <monika.urbanska1@gmail.com>
2019-11-25 12:18:01 +0000

    Figuring out how to count books by keyword

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -24,18 +24,34 @@ const catalogue = [
 
 function countBooks() {
   // Your code here
-}
+  return catalogue.length;
+};
 
 function checkBook(book) {
   // Your code here
-}
+  for (let i = 0; i < catalogue.length; i++) {
+    if (book === catalogue[i]) {
+      return true;
+    }
+    }
+    return false;
+};
 
 function countBooksByFirstLetter(letter) {
   // Your code here
-}
+  // For each item in array, check first letter against given letter
+  const firstLetters = catalogue.map(x => x[0]);
+  let count = 0
+  for (let i = 0; i < firstLetters.length; i++) {
+    if (letter === firstLetters[i]) {
+      count += 1;
+    }
+  } return count;
+};
 
 function countBooksByKeyword(keyword) {
   // Your code here
+  // 
 }
 
 function getBooksByAuthor(author) {
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -6,4 +6,32 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooks()).toBe(20);
     });
   });
+
+  describe("catalogueService.checkBook", () => {
+    test("returns true if the book exists in the list", () => {
+      expect(catalogueService.checkBook("Dracula by Bram Stoker")).toBe(true);
+    });
+
+    test("returns false if the book doesn't exist in the list", () => {
+      expect(catalogueService.checkBook("Moths by Pamela Mothman")).toBe(false);
+    });
+  });
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the number of books beginning with a given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+    });
+
+    test("returns 0 if no books begin with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("X")).toBe(0);
+    });
+  });
+
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns the number of books containing a keyword", () => {
+      expect(catalogueService.countBooksByKeyword("assassin")).toBe(3);
+    });
 });
+});
+
+
